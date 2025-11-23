35c33c1f88597195582da2bf480ebea6ec39e6f7
Sebastian Cardona <bucardona@gmail.com>
2025-11-23 02:02:30 -0500

    update api methods

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -14,23 +14,100 @@ export interface TypeMovie {
   vote_average: number; //6.3;
   vote_count: number; //89;
 }
+export interface TypeMovieDetails extends TypeMovie {
+  genres: TypeGenre[];
+}
+
 export interface TypeGenre {
   id: number; //28;
   name: string; //'Acción';
 }
 
-export async function getPopularMovies(): Promise<TypeMovie[]> {
+export async function getMoviesById(id: number): Promise<TypeMovieDetails> {
   const res = await fetch(
-    'https://api.themoviedb.org/3/movie/popular?api_key=bf42acf712bba686cfff9820897f4edb&language=es-Co&page=1&include_adult=true'
+    `https://api.themoviedb.org/3/movie/${id}?api_key=bf42acf712bba686cfff9820897f4edb&language=es-Co&page=1`
   );
-  const data = await res.json();
-  return data.results;
+  const data: TypeMovieDetails = await res.json();
+  return data;
 }
 
 export async function getGenres(): Promise<TypeGenre[]> {
   const res = await fetch(
     'https://api.themoviedb.org/3/genre/movie/list?api_key=bf42acf712bba686cfff9820897f4edb&language=es-Co&page=1'
   );
-  const data = await res.json();
+  const data: { genres: TypeGenre[] } = await res.json();
   return data.genres;
+}
+
+export async function getPopularMovies(): Promise<TypeMovie[]> {
+  const res = await fetch(
+    'https://api.themoviedb.org/3/movie/popular?api_key=bf42acf712bba686cfff9820897f4edb&language=es-Co&page=1&include_adult=true'
+  );
+  const data: {
+    results: TypeMovie[];
+  } = await res.json();
+  return data.results.filter((movie: TypeMovie) => movie.poster_path !== null);
+}
+
+export async function getMoviesByGenre(
+  genreName: string,
+  genres: TypeGenre[]
+): Promise<TypeMovie[]> {
+  const genre = genres.find((g) => g.name.toLowerCase() === genreName.toLowerCase());
+  if (!genre) {
+    return [];
+  }
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3/discover/movie?api_key=bf42acf712bba686cfff9820897f4edb&language=es-Co&page=1&with_genres=${genre.id}&sort_by=popularity.desc`
+  );
+  const data: {
+    results: TypeMovie[];
+  } = await res.json();
+  return data.results.filter((movie: TypeMovie) => movie.poster_path !== null);
+}
+
+export async function getUpcomingMovies(): Promise<TypeMovie[]> {
+  const res = await fetch(
+    'https://api.themoviedb.org/3/movie/upcoming?api_key=bf42acf712bba686cfff9820897f4edb&language=es-Co&page=2'
+  );
+  let data: {
+    results: TypeMovie[];
+  } = await res.json();
+
+  if (res.ok) {
+    const resSecond = await fetch(
+      'https://api.themoviedb.org/3/movie/upcoming?api_key=bf42acf712bba686cfff9820897f4edb&language=es-Co&page=3'
+    );
+    let dataSecond: {
+      results: TypeMovie[];
+    } = await resSecond.json();
+    if (resSecond.ok) {
+      data.results = data.results.concat(dataSecond.results);
+
+      const resThird = await fetch(
+        'https://api.themoviedb.org/3/movie/upcoming?api_key=bf42acf712bba686cfff9820897f4edb&language=es-Co&page=4'
+      );
+      let dataThird: {
+        results: TypeMovie[];
+      } = await resThird.json();
+      if (resThird.ok) {
+        data.results = data.results.concat(dataThird.results);
+      }
+    }
+  }
+
+  if (res.ok) {
+    const dataUpcoming = data.results;
+    //sort by date ascending
+    dataUpcoming.sort((a: TypeMovie, b: TypeMovie) => {
+      const dateA = new Date(a.release_date);
+      const dateB = new Date(b.release_date);
+      return dateA.getTime() - dateB.getTime();
+    });
+
+    return dataUpcoming.filter((upc: TypeMovie) => new Date(upc.release_date) >= new Date());
+  }
+
+  return data.results.filter((movie: TypeMovie) => movie.poster_path !== null);
 }
